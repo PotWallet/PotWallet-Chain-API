40a88f8860cd8b6d4ba2bfd3e1e1a695a634bd2e
lazouche <info@overio.com>
2016-09-28 13:27:10 -0400

    find/replace Potcoin elements

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ function getUserHome() {
   return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
 
-var home = process.env.INSIGHT_DB || (getUserHome() + '/.reddsight');
+var home = process.env.INSIGHT_DB || (getUserHome() + '/.potchain');
 
 if (process.env.INSIGHT_NETWORK === 'livenet') {
   env = 'livenet';
@@ -56,9 +56,9 @@ var isWin = /^win/.test(process.platform);
 var isMac = /^darwin/.test(process.platform);
 var isLinux = /^linux/.test(process.platform);
 if (!dataDir) {
-  if (isWin) dataDir = '%APPDATA%\\Reddcoin\\';
-  if (isMac) dataDir = process.env.HOME + '/Library/Application Support/Reddcoin/';
-  if (isLinux) dataDir = process.env.HOME + '/.reddcoin/';
+  if (isWin) dataDir = '%APPDATA%\\Potcoin\\';
+  if (isMac) dataDir = process.env.HOME + '/Library/Application Support/Potcoin/';
+  if (isLinux) dataDir = process.env.HOME + '/.potcoin/';
 }
 dataDir += network === 'testnet' ? 'testnet' : '';
 
